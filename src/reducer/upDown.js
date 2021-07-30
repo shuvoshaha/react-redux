366a14def110020b299e4e8315a1ae1aa224bb5b
SKS <shaha460@gmail.com>
2021-07-30 22:07:29 +0600

    create reducer with redux state

---
+++ src/reducer/upDown.js
@@ -0,0 +1,8 @@
+const initalState = 0;
+const changeNumber = (state = initalState, action) =>{
+  switch(action.type){
+      case "INCREMENT": return state + 1 
+      case "DECREMENT" : return state - 1
+
+  }
+}
